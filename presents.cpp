7b8b3f7f168bd2d94daa81ecf1de07933d6cda2c
Pushkar Waykole <pushkarwaykole123@gmail.com>
2022-08-31 22:56:30 +0530

    ..

--- presents.cpp
+++ presents.cpp
@@ -1,35 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define dd float
- 
-#define print(x) cout << x <<endl
 
+#define dd float
+
+#define print(x) cout << x << endl
 
 #define ll long long
 #define vi vector<int>
 #define pb push_back
-#define disp(x) for(auto it:x){cout<<it<<" ";} cout<<endl
- 
- 
+#define disp(x)            \
+    for (auto it : x)      \
+    {                      \
+        cout << it << " "; \
+    }                      \
+    cout << endl
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
- 
+
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
- 
-    int n,a;
+
+
+
+	// Write your code here
+    ll n;
     cin>>n;
-    int arr[501];
-    for(int i=1;i<=n;i++){
-        cin>>a;
-        arr[a]=i;
+    
+    ll count=1;
+   
+    bool ans=false;
+    while(count*(count+1)<n*2){
+    
+        int x = count * (count + 1);
+         
+       
+        int t = n * 2 - x;
+        int k = sqrt(t);
+         
+        
+        if (k * (k + 1) == t)
+        {
+            ans = true;
+            break;
+        }
+        count += 1;
     }
-    for(int i=1;i<=n;i++){
-        cout<<arr[i]<<" ";
+    if(ans){
+        cout<<"YES"<<endl;
     }
+    else{
+        cout<<"NO"<<endl;
+    }
+
 }
